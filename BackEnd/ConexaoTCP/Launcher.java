d2e954ddaf13b8db79c60df4fbd5731f814c482b
João Igor de Sousa Ferro <joaoigorsf990@gmail.com>
2025-12-17 00:05:02 -0300

    Run TCP server in a separate thread with confirmation

--- BackEnd/ConexaoTCP/Launcher.java
+++ BackEnd/ConexaoTCP/Launcher.java
@@ -1,6 +1,19 @@
+import javax.swing.JOptionPane;
+
 public class Launcher {
     public static void main(String[] args) {
         TCPServidor ser = new TCPServidor();
-        ser.iniSer();
+
+        Thread servidorThread = new Thread(() -> ser.iniSer());
+        servidorThread.start();
+
+        int opc = JOptionPane.showConfirmDialog(null,
+                "Deseja parar o servidor?",
+                "Encerrar Servidor",
+                JOptionPane.YES_NO_OPTION);
+
+        if (opc == JOptionPane.YES_OPTION) {
+            System.exit(0);
+        }
     }
 }
