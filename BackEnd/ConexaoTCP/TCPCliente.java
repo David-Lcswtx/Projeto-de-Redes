f5920eb934191bc72c7a76356a382038590cc7e8
João Igor de Sousa Ferro <joaoigorsf990@gmail.com>
2025-12-17 15:27:14 -0300

    Update TCPCliente.java

--- BackEnd/ConexaoTCP/TCPCliente.java
+++ BackEnd/ConexaoTCP/TCPCliente.java
@@ -1,54 +1,38 @@
 import java.net.*;
 import java.io.*;
+import cripto.Criptografia;
 
 public class TCPCliente {
-
-    String hos = "localhost";
-    int por = 5001;
     Socket soc;
     PrintWriter sai;
     BufferedReader ent;
-    Thread thr;
 
     public void conIP(String ip, int por) {
         try {
             soc = new Socket(ip, por);
-
-            sai = new PrintWriter(
-                soc.getOutputStream(), true
-            );
-
-            ent = new BufferedReader(
-                new InputStreamReader(soc.getInputStream())
-            );
-
-            iniThr();
-
+            sai = new PrintWriter(soc.getOutputStream(), true);
+            ent = new BufferedReader(new InputStreamReader(soc.getInputStream()));
         } catch (IOException e) {
             System.err.println("Erro cliente: " + e.getMessage());
         }
     }
 
     public void envMsg(String txt) {
-        sai.println(txt);
+        if (sai != null) {
+            int i = txt.indexOf(":");
+            if (i != -1) {
+                String nome = txt.substring(0, i + 1);
+                String msg = txt.substring(i + 1).trim();
+                String cifrada = Criptografia.criptografar(msg);
+                sai.println(nome + " " + cifrada);
+            } else {
+                sai.println(Criptografia.criptografar(txt));
+            }
+        }
     }
 
     public String recMsg() throws IOException {
         return ent.readLine();
-    }
-
-    private void iniThr() {
-        thr = new Thread(() -> {
-            try {
-                String msg;
-                while ((msg = recMsg()) != null) {
-                    System.out.println("Recebido: " + msg);
-                }
-            } catch (IOException e) {
-                System.err.println("Erro thread: " + e.getMessage());
-            }
-        });
-        thr.start();
     }
 
     public void fecCon() throws IOException {
