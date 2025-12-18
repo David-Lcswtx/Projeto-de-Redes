3a19fa47cd9dca2cadb7addd67c49859206c3d46
João Igor de Sousa Ferro <joaoigorsf990@gmail.com>
2025-12-18 13:38:33 -0300

    Refactor user prompt and server shutdown dialog

--- BackEnd/ConexaoTCP/Launcher.java
+++ BackEnd/ConexaoTCP/Launcher.java
@@ -2,24 +2,19 @@ import javax.swing.JOptionPane;
 
 public class Launcher {
     public static void main(String[] args) {
-        // Pergunta a porta ao usuário
+        
         String portaStr = JOptionPane.showInputDialog("Digite a porta do servidor:");
         int porta = Integer.parseInt(portaStr);
 
-        // Cria servidor com a porta escolhida
         TCPServidor ser = new TCPServidor(porta);
 
-        // Inicia o servidor em uma thread separada
         Thread servidorThread = new Thread(() -> ser.iniSer());
         servidorThread.start();
 
-        // Pergunta se deseja encerrar
-        int opc = JOptionPane.showConfirmDialog(
-                null,
+        int opc = JOptionPane.showConfirmDialog(null,
                 "Deseja parar o servidor?",
                 "Encerrar Servidor",
-                JOptionPane.YES_NO_OPTION
-        );
+                JOptionPane.YES_NO_OPTION);
 
         if (opc == JOptionPane.YES_OPTION) {
             ser.stopServer();
